6e9cdebf3443ecd4d5fe2cdee01927f753480ed3
Aditi More <114819269+aaditi2@users.noreply.github.com>
2023-11-11 15:36:12 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const aditiSide = document.getElementById("aditi");
+const coderSide = document.getElementById("coder");
+
+aditiSide.addEventListener("click", () => {
+    window.location.href = 'posts.html';
+});
+
+coderSide.addEventListener("click", () => {
+    // Redirect to your main website (index.html)
+    window.location.href = 'pmain.html';
+});
